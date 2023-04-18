5da0fd3afb4d3fe16f7bfca07fdfc0e98f11f7ab
juanmz1 <juanmzii48@gmail.com>
2023-04-18 04:15:24 -0700

    task

--- main.c
+++ main.c
@@ -12,11 +12,19 @@ int main (int ac, char **argv)
   char *prompt = "(Ishell) $ ";
   char *lineptr;
   size_t n = 0;
-  
-  printf ("%s", prompt);
-getline(&lineptr, &n, stdin);
- printf("%s\n", lineptr);
- 
-  free (lineptr);
-  return (0);
+  ssize_t n_char;
+
+  while (1)
+  {
+	  printf ("%s", prompt);
+	  n_char = getline(&lineptr, &n, stdin);
+	  printf("%s\n", lineptr);
+	  if (n_char == -1)
+	  {
+		  perror("command not found -> End of File/use of Ctrl+D");
+		  return (-1);
+	  }
+  }
+	  free (lineptr);
+	  return (0);
 }
