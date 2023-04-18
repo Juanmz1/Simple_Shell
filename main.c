d4dd5ec73671d6c7f0b22f81ddc2cc51bf586fbc
juanmz1 <juanmzii48@gmail.com>
2023-04-18 06:02:37 -0700

    task

--- main.c
+++ main.c
@@ -13,18 +13,20 @@ int main (int ac, char **argv)
   char *lineptr;
   size_t n = 0;
   ssize_t n_char;
+  (void)ac; (void)argv;
 
   while (1)
   {
 	  printf ("%s", prompt);
 	  n_char = getline(&lineptr, &n, stdin);
-	  printf("%s\n", lineptr);
 	  if (n_char == -1)
 	  {
-		  perror("command not found -> End of File/use of Ctrl+D");
+		  printf("command not found -> End of File/use of Ctrl+D");
 		  return (-1);
 	  }
-  }
+	  printf("%s\n", lineptr);
+	  
 	  free (lineptr);
-	  return (0);
+  }
+  return (0);
 }
