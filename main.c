e94d26b28ec028933bf9491031ec5d59d7905a00
juanmz1 <juanmzii48@gmail.com>
2023-04-18 08:10:21 -0700

    task

--- main.c
+++ main.c
@@ -9,24 +9,61 @@
 
 int main (int ac, char **argv)
 {
-  char *prompt = "(Ishell) $ ";
-  char *lineptr;
-  size_t n = 0;
-  ssize_t n_char;
-  (void)ac; (void)argv;
+	char *prompt = "(Ishell) $ ";
+	char *lineptr = NULL, *lineptr_cpy = NULL;
+	const char *deli = " \n";
+	size_t n = 0;
+	ssize_t n_char;
+	char *token;
+	int i;
+	int token_count;
+	(void)ac;
+	
+	while (1)
+	{
+		printf ("%s", prompt);
+		n_char = getline(&lineptr, &n, stdin);
+		if (n_char == -1)
+		{
+			printf("command not found -> End of File/use of Ctrl+D");
+			return (-1);
+		}
+		//memory allocation of the lineptr_cpy//
+		lineptr_cpy = malloc(sizeof(char) *n_char);
+		if (lineptr_cpy == NULL)
+		{
+			perror("tsh: memory allocation error");
+			return (-1);
+		}
+		// copy the lineptr to the lineptr_cpy//
+		strcpy(lineptr_cpy, lineptr);
+		// calculate the number of token//
+		token = strtok(lineptr, deli);
+		while (token != NULL)
+		{
+			token_count++;
+			token = strtok(NULL, deli);
+		}
+		token_count++;
+		// allocate a space to hold the array of string
+		argv = malloc(sizeof(char *) *token_count);
+		// store the token in the array
+		token = strtok(lineptr_cpy, deli);
+		for (i = 0; token != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) *strlen(token));
+			strcpy(argv[i], token);
+			token = strtok(NULL, deli);
+		}
+		argv[i] = NULL;
+		printf("%s\n", lineptr);
+		for (i = 0; i < token_count - 1; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
 
-  while (1)
-  {
-	  printf ("%s", prompt);
-	  n_char = getline(&lineptr, &n, stdin);
-	  if (n_char == -1)
-	  {
-		  printf("command not found -> End of File/use of Ctrl+D");
-		  return (-1);
-	  }
-	  printf("%s\n", lineptr);
-	  
-	  free (lineptr);
-  }
-  return (0);
+		free(lineptr);
+		free(lineptr_cpy);
+	}
+	return (0);
 }
