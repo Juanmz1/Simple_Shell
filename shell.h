69a64a45f47195f24d580dc40091316c7f52cd2b
juanmz1 <juanmzii48@gmail.com>
2023-04-22 05:44:31 -0700

    task

--- shell.h
+++ shell.h
@@ -20,17 +20,7 @@ char **token_cmd(char *lineptr_cpy);
 extern char **environ;
 void excecmd(char **arr);
 int main(int ac, char *argv[]);
-char *fcheck(char *cmd);
-int pcheck(char *cmd);
-<<<<<<< HEAD
 char *togetenv(char *cmd);
 char *print_env(char *cmd);
-=======
-char *args[MAX_ARGS];
-char arg_buffer[MAX_ARGUMENTS];
-setenv("MY_VAR", "my_value", 1);
-int cd(char **args)
-  
->>>>>>> 444c888ce657d4dd1dbf918bd7a3a621ad9f4436
 
 #endif
