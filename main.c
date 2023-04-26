caecf0057d9a18bcb18b9220fd590aa0703c736d
Fayth7 <flexflakes@gmail.com>
2023-04-26 02:05:57 -0700

    main.c update

--- main.c
+++ main.c
@@ -6,43 +6,43 @@
  * main - entry point
  * @ac: arg count
  * @argv: arg vector
- * @envp: array of environment variables
- * Return: 0 
+ * Return: 0
 */
-#define MAX_COMMANDS 10
-int main (int ac, char *argv[])
+
+int main(int ac, char *argv[])
+
 {
-	char **string;
-	char *prompt = "Ishell#$ ";
-	char *lineptr = NULL;
-	size_t n = 0;
-	ssize_t n_char;
+char **string;
+char *prompt = "Ishell#$ ";
+char *lineptr = NULL;
+size_t n = 0;
+ssize_t n_char;
 
-	if (ac > 1)
-		argv[1] = NULL;
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			printf("%s", prompt);
-		lineptr = malloc(sizeof(char) * n);
-		n_char = my_getline(&lineptr, &n, stdin);
-		if (n_char == -1)
-		{
-			printf("EXITING SHELL");
-			return (-1);
-		}
-		if (*lineptr != '\n')
-		{
-			string = token_cmd(lineptr);
-			if (strcmp("exit",string[0]) == 0)
-				break;
-			if (strcmp("exit", string[0]) == 0)
-				exit_shell(EXIT_SUCCESS);
-			excecmd(string);
-		}
-	}
-	free(lineptr);
-	free(string);
+if (ac > 1)
+argv[1] = NULL;
+while (1)
+{
+if (isatty(STDIN_FILENO))
+i_printf("%s", prompt);
+lineptr = malloc(sizeof(char) * n);
+n_char = my_getline(&lineptr, &n, stdin);
+if (n_char == -1)
+{
+i_printf("EXITING SHELL");
+return (-1);
+}
+if (*lineptr != '\n')
+{
+string = token_cmd(lineptr);
+if (strcmp("exit", string[0]) == 0)
+break;
+if (strcmp("exit", string[0]) == 0)
+exit_shell(EXIT_SUCCESS);
+excecmd(string);
+}
+}
+free(lineptr);
+free(string);
 
-	return (0);
+return (0);
 }
