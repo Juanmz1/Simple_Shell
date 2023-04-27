5a70a62d16ace4ee050fe5f5edc89e4eecaf37be
juanmz1 <juanmzii48@gmail.com>
2023-04-27 02:18:13 -0700

    task

--- shell.h
+++ shell.h
@@ -16,8 +16,12 @@
 #include <stddef.h>
 #include <fcntl.h>
 #include <errno.h>
+
+
+
 #define MAX_ARGS 10
 #define MAX_ARGUMENTS 100
+
 void exit_shell(int status);
 char **token_cmd(char *lineptr_cpy);
 extern char **environ;
@@ -25,30 +29,5 @@ void excecmd(char **arr);
 int main(int ac, char *argv[]);
 char *togetenv(char *cmd);
 char *print_env(char *cmd);
-size_t my_getline(char **lineptr, size_t *n, FILE *stream);
-int i_printf(const char *format, ...);
-typedef struct passinfo
-{
-	char *arg;
-	char **argv;
-	char *path;
-	int argc;
-	unsigned int line_count;
-	int err_num;
-	int linecount_flag;
-	char *fname;
-	list_t *env;
-	list_t *history;
-	list_t *alias;
-	char **environ;
-	int env_changed;
-	int status;
-	int readfd;
-	int histcount;
-	int cmd_buf_type;
-} info_t;
-
-ssize_t get_input(info_t *);
-void sigHand(int);
 
 #endif
